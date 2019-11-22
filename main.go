d429fdbbcc2186837d3521f01d7b45dfe0a1809e
rusa gedougawa <rusa.gedougawa@gmail.com>
2019-11-22 18:01:25 +0900

    SequenceToken対応

--- main.go
+++ main.go
@@ -37,19 +37,27 @@ func Loop() {}
 
 func PutLog(group string, stream string, message string, sess *session.Session) {
 	svc := cloudwatchlogs.New(sess)
-	//token := ConfirmSequenceToken(group, stream)
+	token := ConfirmSequenceToken(group, stream, sess)
 
 	configInputLogEvent := new(cloudwatchlogs.InputLogEvent).
 		SetMessage(message).
 		SetTimestamp(aws.TimeUnixMilli(time.Now()))
 
 	configInputLogEventList := []*cloudwatchlogs.InputLogEvent{configInputLogEvent}
+	configPutLogEventsInput := new(cloudwatchlogs.PutLogEventsInput)
 
-	configPutLogEventsInput := new(cloudwatchlogs.PutLogEventsInput).
-		SetLogEvents(configInputLogEventList).
-		SetLogGroupName(group).
-		SetLogStreamName(stream)
-		//SetSequenceToken(token)
+	if token == "" {
+		configPutLogEventsInput = configPutLogEventsInput.
+			SetLogEvents(configInputLogEventList).
+			SetLogGroupName(group).
+			SetLogStreamName(stream)
+	} else {
+		configPutLogEventsInput = configPutLogEventsInput.
+			SetLogEvents(configInputLogEventList).
+			SetLogGroupName(group).
+			SetLogStreamName(stream).
+			SetSequenceToken(token)
+	}
 
 	_, err := svc.PutLogEvents(configPutLogEventsInput)
 
@@ -58,17 +66,32 @@ func PutLog(group string, stream string, message string, sess *session.Session) 
 	}
 }
 
-func ConfirmSequenceToken(group string, stream string) string {
-	return ""
+func ConfirmSequenceToken(group string, stream string, sess *session.Session) string {
+	svc := cloudwatchlogs.New(sess)
+
+	configDescribeLogStreamsInput := new(cloudwatchlogs.DescribeLogStreamsInput).
+		SetLogGroupName(group).
+		SetLogStreamNamePrefix(stream)
+
+	res, err := svc.DescribeLogStreams(configDescribeLogStreamsInput)
+	if err != nil {
+		panic(err)
+	}
+
+	if res.LogStreams[0].UploadSequenceToken == nil {
+		return ""
+	} else {
+		return *res.LogStreams[0].UploadSequenceToken
+	}
 }
 
 func CreateLogStream() {
 }
 
 func main() {
-	Profile = "test"
+	Profile = "trfm"
 	Region = "ap-northeast-1"
-	LogGroup = "test"
+	LogGroup = "test2"
 	LogStream = "go"
 	LogMessage = "hello go"
 	fmt.Println(LogMessage)
